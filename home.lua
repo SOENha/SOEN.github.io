7cee75aab862c8357e8b93eab2973ed2105e6d17
SOENha <shaochuyuan@gmail.com>
2024-05-16 01:33:16 +0900

    Create home.lua

---
+++ home.lua
@@ -0,0 +1,20 @@
+SOEN.github.io/
+│
+├── index.html
+├── entrance.html
+├── livingroom.html
+├── kitchen.html
+├── bathroom.html
+├── studio.html
+├── bedroom.html
+│
+├── css/
+│   └── styles.css
+│
+├── js/
+│   └── script.js
+│
+├── images/
+    ├── heaven/
+    ├── hell/
+    └── earth/
